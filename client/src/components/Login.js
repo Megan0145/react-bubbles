3410c2deefd0453a084a1731bbb2975c293122d8
Susan Ennis <susanennis@Tonys-MacBook.local>
2019-10-18 08:38:24 +0100

    Login form & login post request to handle token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const submit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login",{
+      username: usernameRef.current.value,
+      password: passwordRef.current.value
+    })
+    .then(res => {
+     localStorage.setItem('token', res.data.payload);
+     console.log(props)
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form>
+        <input ref={usernameRef} placeholder='Username' type='text'/>
+        <input ref={passwordRef} placeholder='Password' type='text'/>
+        <button onClick={submit}>Login</button>
+      </form>
+    </div>
   );
 };
 
